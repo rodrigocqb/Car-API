547c2229be818b91d0ac7a31aa3de145b4d99f1b
rodrigocqb <rodrigocortibarros@gmail.com>
2023-04-13 23:16:55 -0300

    feat: create delete request for cars

--- src/main/java/com/cars/api/controller/CarsController.java
+++ src/main/java/com/cars/api/controller/CarsController.java
@@ -3,7 +3,9 @@ package com.cars.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +32,10 @@ public class CarsController {
     @PostMapping
     public void create(@RequestBody @Valid CarDTO req) {
         repository.save(new Car(req));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCar(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
